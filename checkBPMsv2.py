85b8d9e5305137886e598ea160f1087be52bead6
adamkoval <adam_koval@hotmail.com>
2019-06-11 12:12:28 +0200

    run.sad now looks for the correct async BPM file.

---
+++ checkBPMsv2.py
@@ -0,0 +1,19 @@
+"""
+Created on Tue Jun 11
+Author: Adam Koval
+"""
+from __future__ import print_function
+import argparse
+
+# Argument parser.
+parser = argparse.ArgumentParser()
+parser.add_argument('--axis', '-ax',
+                    dest='axis',
+                    action='store')
+parser.add_argument('--async_output_dir',
+                    dest='aod',
+                    action='store')
+args = parser.parse_args()
+
+# All else
+
